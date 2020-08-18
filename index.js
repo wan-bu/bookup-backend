421217da3b9063b0bd8ccae4c47b74b7b0f579db
AhmedL-dev <laadraoui.ahmed@gmail.com>
2020-08-18 14:47:02 +0100

    init

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const mongoose = require("mongoose");
-
+const cors = require("cors");
 const app = express();
+
+app.use(cors());
 
 require("./startup/routes")(app);
 require("./startup/db")();
--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,11 @@ const User = require("../models/user");
 const express = require("express");
 const router = express.Router();
 
+router.get("/", async (req, res) => {
+  users = await User.find();
+  res.send(users);
+});
+
 router.post("/", async (req, res) => {
   user = await new User(req.body);
 
